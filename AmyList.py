da9f55473388231aab6d8735596b28df3c6eb107
Oscar Sandford <oscarsandford@users.noreply.github.com>
2020-05-26 21:42:24 -0700

    get a profile's favourite stories, give links to story class

--- AmyList.py
+++ AmyList.py
@@ -13,6 +13,8 @@ class Story():
         self.rank = int(soup.find('span', attrs={'class':'numbers ranked'}).find('strong').get_text().split('#')[1])
         self.episodes = int(soup.find_all(class_='spaceit')[3].get_text().split('/')[1])
 
+    def get_title(self):
+        return self.title
 
     def __str__(self):
         s = 'Title: ' + self.title
@@ -24,8 +26,30 @@ class Story():
 
 class Profile():
 
+    def favourite_stories(self, category):
+        stories = []
+        for entry in category.find_all(class_='di-tc va-t pl8 data'):
+            for link in entry.find_all('a', href=True):
+                # got the link!
+                #print(link['href'])
+                story = Story(str(link['href']))
+                stories.append(story)
+
+        return stories
+
+    def favourite_beings(self):
+        pass
+
     def __init__(self, url):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
+        category = ['anime', 'manga', 'characters', 'people']
+
+        anime = soup.find('ul', attrs={'class':'favorites-list '+category[0]})
+        self.favourite_anime = self.favourite_stories(anime)
 
         
+
+    def get_faves(self):
+        for anime in self.favourite_anime:
+            print(anime.get_title())
--- client.py
+++ client.py
@@ -2,7 +2,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-from AmyList import Story
+from AmyList import Story, Profile
+
+user = 'amykyst'
 
 show = Story('https://myanimelist.net/anime/205/Samurai_Champloo')
 print(show)
+
+user_profile = Profile('https://myanimelist.net/profile/' + user)
+
+print(user_profile.get_faves())
