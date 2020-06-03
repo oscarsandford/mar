f9d0bec1bf4bc728d4f88e25e95d193f21aabea4
Oscar Sandford <oscarsandford@gmail.com>
2020-06-03 00:50:08 -0700

    made export more modular, string formats more concise, deleted txts with old name scheme

--- AmyList.py
+++ AmyList.py
@@ -3,141 +3,119 @@ from bs4 import BeautifulSoup
 
 class Story():
 
-    # Given URL, retrieve story page soup
-    def __init__(self, url):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        self.title, self.score, self.rank, self.episodes, self.my_rating = 'error', 0.0, 0, 0, 0
-        # Any errors in gathering story data will make story invalid
-        try:
-            # TODO: find a better way to get the title from h1-title
-            t_div = soup.find(class_='spaceit_pad')
-            self.title = t_div.get_text().split(t_div.find('span', {'class':'dark_text'}).get_text())[1].replace('\n', '')
-            print("Appending . . . ", self.title)
+	# Given URL, retrieve story page soup
+	def __init__(self, url):
+		response = requests.get(url)
+		soup = BeautifulSoup(response.text, "html.parser")
+		self.title, self.score, self.rank, self.episodes, self.my_rating = " error", 0.0, 0, 0, 0
+		try:
+			# TODO: find a better way to get the title from h1-title
+			t_div = soup.find(class_="spaceit_pad")
+			self.title = t_div.get_text().split(t_div.find("span", {"class":"dark_text"}).get_text())[1].replace("\n", "")
+			print("Appending . . . ", self.title)
 
-            s_div = soup.find('div', {'class':'fl-l score'}).get_text()
-            if s_div != 'N/A':
-                self.score = float(s_div)
+			s_div = soup.find("div", {"class":"fl-l score"}).get_text()
+			if s_div != "N/A":
+				self.score = float(s_div)
 
-            r_div = soup.find('span', {'class':'numbers ranked'}).find('strong').get_text()
-            if r_div != 'N/A':
-                self.rank = int(r_div.split('#')[1])
+			r_div = soup.find("span", {"class":"numbers ranked"}).find("strong").get_text()
+			if r_div != "N/A":
+				self.rank = int(r_div.split("#")[1])
 
-            self.episodes = int(soup.find_all(class_='spaceit')[3].get_text().split('/')[1])
+			self.episodes = int(soup.find_all(class_="spaceit")[3].get_text().split("/")[1])
 
-        except Exception as e:
-            print('Something wrong with story..\t', url)
-            print('Exception: ', e,'\n')
+		except Exception as e:
+			print("Something wrong with story..\t", url)
+			print("Exception: ", e,"\n")
 
 
-    def get_title(self):
-        return self.title
+	def get_title(self):
+		return self.title
 
-    def get_score(self):
-        return self.score
+	def get_score(self):
+		return self.score
 
-    def get_rank(self):
-        return self.rank
+	def get_rank(self):
+		return self.rank
 
-    def get_episodes(self):
-        return self.episodes
+	def get_episodes(self):
+		return self.episodes
 
-    def set_my_rating(self, score):
-        self.my_rating = score
+	def set_my_rating(self, num):
+		self.my_rating = num
 
-    def get_my_rating(self):
-        return self.my_rating
+	def get_my_rating(self):
+		return self.my_rating
 
-    def __str__(self):
-        s = '\nTitle:' + self.title
-        s += '\nScore: ' + str(format(self.score, '.2f'))
-        s += '\nRank: #' + str(self.rank)
-        s += '\nEpisodes: ' + str(self.episodes)
-        return s
+	def format_story(self):
+		return "\nTitle:" + self.title + "\nUser Rating: " + str(self.my_rating) + "\n"
+
+	def __str__(self):
+		s = "\nTitle:" + self.title
+		s += "\nScore: " + str(format(self.score, ".2f"))
+		s += "\nRank: #" + str(self.rank)
+		s += "\nEpisodes: " + str(self.episodes)
+		return s
 
 
 class Profile():
 
-    # Given MAL username, retrieve user profile page data
-    def __init__(self, username):
-        self.username = username
-
-        # Set lists (watching, completed, ptw, etc.)
-        self.anime_list = self.all_stories('anime')
-
-    # Find user's favourite anime or manga
-    # TODO: deprecate this at some point
-    def favourite_stories(self, category):
-        url = 'https://myanimelist.net/profile/' + self.username
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        list = soup.find('ul', attrs={'class':'favorites-list '+category})
-
-        stories = []
-        # Try if user has a list of favourites in this category
-        try:
-            for entry in list.find_all(class_='di-tc va-t pl8 data'):
-                for link in entry.find_all('a', href=True):
-                    # got the link!
-                    story = Story(str(link['href']))
-                    stories.append(story)
-        # If not, return an empty list
-        except Exception as e:
-            print(e + ': user does not have a favourite '+category +' list!')
-
-        return stories
+	# Given MAL username, retrieve user profile page data
+	def __init__(self, username):
+		self.username = username
+		self.anime_list = self.all_stories("anime")
+		self.manga_list = self.all_stories("manga")
 
 
-    # Find all anime from user list
-    def all_stories(self, category):
-        url = 'https://myanimelist.net/'+category+'list/'+self.username
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        stories = []
-        # This try is for avoiding random 403's
-        try:
-            # Get rid of the other table contents, isolate data
-            contents = soup.find('table', {'class':'list-table'})
-            for t in contents('tbody'):
-                t.decompose()
-            data = contents['data-items']
-            # Sort out and make stories with the urls
+	def all_stories(self, category):
+		url = "https://myanimelist.net/"+category+"list/"+self.username
+		response = requests.get(url)
+		soup = BeautifulSoup(response.text, "html.parser")
+		stories = []
 
-            links = data.split(category + '_url\":\"')
-            scores = data.split('\"score\":')
-            assert len(links) == len(scores)
+		contents = soup.find("table", {"class":"list-table"})
+		for t in contents("tbody"):
+			t.decompose()
+		data = contents["data-items"]
 
-            for i in range(1, len(links)):
-                # Split on last " and remove redundant backslashes
-                link = links[i].split('\"')[0].replace('\\', '')
-                score = int(scores[i].split('\"')[0].replace(',', ''))
-                story = Story('https://myanimelist.net' + link)
-                story.set_my_rating(score)
-                stories.append(story)
+		links = data.split(category + "_url\":\"")
+		scores = data.split("\"score\":")
+		assert len(links) == len(scores)
 
-        except Exception as e:
-            print(e + ': myanimelist.net is down again!')
+		for i in range(1, len(links)):
+			link = links[i].split("\"")[0].replace("\\", "")
+			score = int(scores[i].split("\"")[0].replace(",", ""))
+			story = Story("https://myanimelist.net" + link)
+			story.set_my_rating(score)
+			stories.append(story)
 
-        return stories
+		return stories
 
-    def get_fave_anime(self):
-        return self.favourite_anime
 
-    def get_fave_manga(self):
-        return self.favourite_manga
+	def get_list(self, category):
+		if category == "manga":
+			return self.manga_list
+		else:
+			return self.anime_list
 
-    # TODO: add an arg to change for different display options
-    def get_anime_list(self):
-        s = ''
-        for anime in self.anime_list:
-            s += str(anime) + '\n(User Score: ' + str(anime.get_my_rating()) + ')\n'
-        return s
 
-    def export_anime_list(self):
-        filename = 'mal_' + self.username + '.txt'
-        txt_file = open(filename, 'w', errors='replace')
-        s = ''
-        for anime in self.anime_list:
-            s = str(anime) + '\n(User Score: ' + str(anime.get_my_rating()) + ')\n'
-            txt_file.write(s)
-        txt_file.close()
+	def build_list(list):
+		for story in list:
+			s += story.format_story()
+		return s
+
+
+	def get_list_string(self, category):
+		if category == "manga":
+			return build_list(self.manga_list)
+		else:
+			return build_list(self.anime_list)
+
+
+	def export_list(self, category):
+		filename = "mal_" + category + "_" + self.username + ".txt"
+		txt_file = open(filename, "w", errors="replace")
+		txt_file.write(self.username + "\'s " + category + " list:\n")
+		for story in self.get_list(category):
+			txt_file.write(story.format_story())
+		txt_file.close()
--- client.py
+++ client.py
@@ -2,20 +2,15 @@
 
 from AmyList import Story, Profile
 
-user1 = 'amykyst'
-user2 = 'winstonmanatee'
-user3 = 'lanzer001'
-user4 = 'frosty114'
+user1 = "amykyst"
+user2 = "winstonmanatee"
+user3 = "lanzer001"
+user4 = "frosty114"
 
 def main():
-    user_profile = Profile(user1)
-
-    li = user_profile.get_anime_list()
-    print(li)
-
-    # Put this info in a txt file
-    user_profile.export_anime_list()
-
+	user = Profile(user4)
+	user.export_list("anime")
+	user.export_list("manga")
 
 if __name__ == '__main__':
     main()
