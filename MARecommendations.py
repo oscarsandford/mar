caeb8b329516a5de3ae69060da33e500b66789ec
Oscar Sandford <oscarsandford@gmail.com>
2020-06-05 00:01:42 -0700

    cleaned up client calls and added basics to recommendations

--- MARecommendations.py
+++ MARecommendations.py
@@ -2,3 +2,9 @@ class Recommendations():
 
 	def __init__(self, profile):
 		self.profile = profile
+		self.anime_recmd = []
+		self.manga_recmd = []
+
+	def recommend(self, category):
+		# TODO: recommend stories based on the profile
+		pass
--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from MAList import Story, Profile
+from MARecommendations import Recommendations
 
 user1 = "amykyst"
 user2 = "winstonmanatee"
@@ -9,12 +10,18 @@ user4 = "frosty114"
 
 def main():
 	profile = make_user(user4)
+	make_recommendations(profile)
 
-def make_user(user):
-	p = Profile(user)
+def make_user(u):
+	p = Profile(u)
 	p.set_list("anime")
 	p.export_list("anime")
 	return p
+
+def make_recommendations(p):
+	r = Recommendations(p)
+	r.recommend("anime")
+
 
 if __name__ == '__main__':
     main()
