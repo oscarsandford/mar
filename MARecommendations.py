e1ba1a8f377505787d07f0461ba5314fcfa45a30
Oscar Sandford <oscarsandford@gmail.com>
2020-09-07 12:08:44 -0700

    Fixed infinite loop when not enough recommendations found

--- MARecommendations.py
+++ MARecommendations.py
@@ -21,21 +21,20 @@ class Recommendations():
 
 	# Given an existing list and parameters from the client, choose random stories
 	# from which to draw recommendations from. Add these stories to the class's list.
-	# (TODO: min_score currently not used, but will likely be!)
-	def recommend(self, user_list, min_score, result_count):
-
+	def recommend(self, user_list, target_count, max_attempts):
 		start = time.time()
 		tmp = []
 		used_links = [[]]
+		attempt_count = 0
 
-		while len(tmp) < result_count:
+		while len(tmp) < target_count and attempt_count < max_attempts:
 
 			rand_index = random.randint(0,len(user_list)-1)
 			rand_link = user_list[rand_index][LINK_INDEX]
 
 			if rand_link not in used_links:
 				used_links.append(rand_link)
-				page_recs = self.get_page_recommendation_info(rand_link)
+				page_recs = self.get_page_recommendations(rand_link, max_attempts)
 				
 				# Although we iterate through the use list a lot here, I have tried to
 				# sort the list by link code leading digit, and only look at leading digit
@@ -48,19 +47,20 @@ class Recommendations():
 						if suggestion[LINK_INDEX] == existing_item[LINK_INDEX]:
 							already_exists = True
 							break
-					if not already_exists and len(tmp) < result_count and suggestion not in tmp:
+					if not already_exists and len(tmp) < target_count and suggestion not in tmp:
 						tmp.append(suggestion)
 						print("\t\033[2m. . . \033[0m"+suggestion[TITLE_INDEX])
 
-		self.set_recommendations(tmp)
+			attempt_count += 1
 
+		self.set_recommendations(tmp)
 		end = time.time()
 		print("\t(\033[92m@\033[0m) Recommendations compiled in \033[96m" + str(format(end-start, "0.4f")) + "\033[0m seconds.")
 		
 
 	# Given a story URL, grab the recommendations list data.
 	# Return a list of [title, link] pairs.
-	def get_page_recommendation_info(self, url):
+	def get_page_recommendations(self, url, max_attempts):
 		response = requests.get(url)
 		soup = BeautifulSoup(response.text, "html.parser")
 		recs = []
@@ -68,28 +68,46 @@ class Recommendations():
 		try:
 			section = soup.find("ul", {"class":"anime-slide js-anime-slide"})
 			all_recommended = section.find_all("a", {"class":"link bg-center"})
-			tries = 0
+			page_code = url.split("/")[4]
+			attempt_count = 0
 
 			# Counting the number of tries prevents infinite loop, checking length
 			# of all recommendations here prevents negative in randint function call.
-			while len(recs) <= 3 and len(all_recommended) != 0 and tries < 10:
+			while len(recs) <= 3 and len(all_recommended) != 0 and attempt_count < max_attempts:
 				rand_index = random.randint(0, len(all_recommended)-1)
 				r = all_recommended[rand_index]
 				link = r.get("href")
 
-				# Edge case where recommendations is in the url for some reason
+				# Dummy example link: "https://myanimelist.net/recommendations/anime/1-2"
+				# is referenced in these comments
+
+				# Common case where recommendations is in the url for some reason
 				if "recommendations" in link:
 					link = link.replace("recommendations/", "")
+				
+				# Either the recommended story has its code on 
+				# the left or right side of the hyphen
+				codes = link.split("/")[4].split("-")
+				link_code_0 = link_code_1 = link.split("/")[4].split("-")[0]
 
-				link_code = link.split("/")[4].split("-")[0]
+				# Only reassign if the dummy link case is present. Sometimes
+				# we have "https://myanimelist.net/recommendations/anime/1" only.
+				# In this case, we don't reassign.
+				if len(codes) > 1:
+					link_code_1 = link.split("/")[4].split("-")[1]
 
-				# Compare page code against potential links
-				if url.split("/")[4] != link_code:
-					title = r.find("span", {"class":"title fs10"}).get_text()
-					link = "https://myanimelist.net/"+self.category+"/"+link_code
-					recs.append([title, link])
+				title = r.find("span", {"class":"title fs10"}).get_text()
+				link = "https://myanimelist.net/"+self.category+"/"
 
-				tries += 1
+				# Compare the page's code against the possible codes. So if 
+				# the page's code was 1, we take the elif case since 2 is 
+				# on the right side and 1 is on the left, and 1==1.
+				if page_code != link_code_0:
+					recs.append([title, link+link_code_0])
+				elif page_code != link_code_1:
+					recs.append([title, link+link_code_1])
+				
+				attempt_count += 1
 
 		except Exception as e:
 			print("Page recommendation went wrong! ("+url+")\n Exception: ", e)
